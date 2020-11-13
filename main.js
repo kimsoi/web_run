97ed18162b22c53af0ffb9703602e20b5a4e4ff4
DESKTOP-6MOBCAM\김소2 <soyi0507@naver.com>
2020-11-13 21:15:02 +0900

    for문으로 index번호 붙이기

---
+++ main.js
@@ -0,0 +1,10 @@
+(() => {
+    /*전역변수 쓰지 않기위해 */
+
+    const stepElems = document.querySelectorAll('.step');
+    const graphicElms = document.querySelectorAll('.graphic-item');
+
+    for (le i =0; i< stepElems.length; i++){
+        stepElems[i].setAttribute('data-index',i);
+    }
+})();
