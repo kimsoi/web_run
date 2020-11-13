3e0b615202e7d5c02735700f745d322b176ae6cf
DESKTOP-6MOBCAM\김소2 <soyi0507@naver.com>
2020-11-13 21:17:42 +0900

    index이름

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
     const graphicElms = document.querySelectorAll('.graphic-item');
 
     for (let i =0; i< stepElems.length; i++){
-        stepElems[i].setAttribute('data-index',i);
+        // stepElems[i].setAttribute('data-index',i);
+        stepElems[i].CDATA_SECTION_NODE.index //index는 이름 지은 것 
     }
 })();
